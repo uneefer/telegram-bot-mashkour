0c03f3d152710d31ecad33c4416b021e714f819d
uneefer <Uneefer@protonmail.com>
2025-06-08 11:59:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import telebot
 import os
+import time
 
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "سلام! پیام خود را بفرست تا به فرمت خاص با عکس پاسخ دهم.")
+try:
+    @bot.message_handler(commands=['start'])
+    def send_welcome(message):
+        bot.reply_to(message, "سلام! پیام خود را بفرست تا بات پاسخ دهد.")
 
-@bot.message_handler(func=lambda m: True)
-def handle_text(message):
-    text = message.text
-    final_message = f"📌 *دسته‌بندی*: 🧠 فلسفه\n\n{text}\n\n#فلسفه #telegram"
-    with open("image.jpg", "rb") as photo:
-        bot.send_photo(message.chat.id, photo, caption=final_message, parse_mode="Markdown")
+    @bot.message_handler(func=lambda m: True)
+    def handle_text(message):
+        text = message.text
+        final_message = f"📌 *دسته‌بندی*: 🧠 فلسفه\n\n{text}\n\n#فلسفه #telegram"
+        with open("image.jpg", "rb") as photo:
+            bot.send_photo(message.chat.id, photo, caption=final_message, parse_mode="Markdown")
 
-bot.infinity_polling()
+    bot.infinity_polling()
+
+except Exception as e:
+    print(f"Error: {e}")
+    time.sleep(15)
